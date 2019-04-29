b66d28ee29348579e3adf5e18a6f3a15157bdb6e
Nathan <nathan.park@sendbird.com>
2019-04-29 18:10:12 +0900

    Add relay server

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws, request) {
+  console.log("connected. " + request.connection.remoteAddress);
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+    wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+  });
+});
---
+++ test_client.js
@@ -0,0 +1,40 @@
+var WebSocket = require('ws');
+var readline = require('readline')
+
+var r = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+r.setPrompt('> ');
+r.on('line', function(line){
+    if (line == 'exit') {
+        r.close();
+    }
+
+    client.send(line);
+    r.prompt()
+});
+
+r.on('close', function(){
+    process.exit();
+});
+ 
+var client = new WebSocket('ws://localhost:8080/');
+client.on('open', function() {
+    console.log("open");
+    r.prompt();
+});
+
+client.on('message', function incoming(message) {
+    console.log(message);
+});
+
+client.on('ping', function() {
+    console.log("ping");
+});
+
+client.on('close', function() {
+    console.log("clear");
+    process.exit();
+});
+
