a922c3badb65fe3879234a8674a0b0118055351f
Nathan <nathan.park@sendbird.com>
2019-04-30 10:32:09 +0900

    Add log

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 5000 });
 
+console.log('process.env.PORT = ' + process.env.PORT);
+
 wss.on('connection', function connection(ws, request) {
   console.log("connected. " + request.connection.remoteAddress);
   ws.on('message', function incoming(message) {
